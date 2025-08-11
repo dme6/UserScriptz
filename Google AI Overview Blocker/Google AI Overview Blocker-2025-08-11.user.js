4c857b4ec388ba4c1cb5eabdef98a0898449a414
dme6 <dme6mail@gmail.com>
2025-08-10 23:02:12 -0600

    This is probably better practice

--- Google AI Overview Blocker/Google AI Overview Blocker-2025-08-11.user.js
+++ Google AI Overview Blocker/Google AI Overview Blocker-2025-08-11.user.js
@@ -14,49 +14,53 @@
 (function() {
     'use strict';
 
-    const State = {
-        SHOWN: 0,
-        HIDDEN: 1
-    };
-
-    let eState = State.HIDDEN;
-
     // const e = document.getElementsByClassName("hdzaWe")[0];
     const e = document.getElementById("eKIzJc");
-    const div = document.createElement("div");
-    const btn = document.createElement("button");
 
-    e.style.display = "none";
+    if(e){
 
-    div.style.backgroundColor = "rgba(255, 255, 255, 0.08)";
-    div.style.borderRadius = "8px";
-    div.style.marginBottom = "20px";
-    div.style.marginTop = "10px";
+        const div = document.createElement("div");
+        const btn = document.createElement("button");
 
-    btn.style.marginLeft = "10px";
-    btn.style.marginTop = "10px";
-    btn.style.marginBottom = "10px";
-    btn.style.padding = "5px";
-    btn.innerText = "Show AI Results";
+        const State = {
+            SHOWN: 0,
+            HIDDEN: 1
+        };
 
-    div.appendChild(btn);
-    e.parentElement.appendChild(div)
+        let eState = State.HIDDEN;
 
-    btn.addEventListener("click", ev => {
-        switch(eState){
-            case State.HIDDEN: {
-                e.style.display = "block";
-                btn.innerText = "Hide AI Results";
-                eState = State.SHOWN;
-                break;
+        e.style.display = "none";
+
+        div.style.backgroundColor = "rgba(255, 255, 255, 0.08)";
+        div.style.borderRadius = "8px";
+        div.style.marginBottom = "20px";
+        div.style.marginTop = "10px";
+
+        btn.style.marginLeft = "10px";
+        btn.style.marginTop = "10px";
+        btn.style.marginBottom = "10px";
+        btn.style.padding = "5px";
+        btn.innerText = "Show AI Results";
+
+        div.appendChild(btn);
+        e.parentElement.appendChild(div)
+
+        btn.addEventListener("click", ev => {
+            switch(eState){
+                case State.HIDDEN: {
+                    e.style.display = "block";
+                    btn.innerText = "Hide AI Results";
+                    eState = State.SHOWN;
+                    break;
+                }
+                case State.SHOWN: {
+                    e.style.display = "none";
+                    btn.innerText = "Show AI Results";
+                    eState = State.HIDDEN;
+                    break;
+                }
             }
-            case State.SHOWN: {
-                e.style.display = "none";
-                btn.innerText = "Show AI Results";
-                eState = State.HIDDEN;
-                break;
-            }
-        }
-    });
+        });
+    }
 
 })();
