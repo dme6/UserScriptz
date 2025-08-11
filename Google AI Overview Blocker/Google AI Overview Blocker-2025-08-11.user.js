dbf9c2a4322f4be4457a0c3de5c49e54c5347512
dme6 <dme6mail@gmail.com>
2025-08-11 01:18:53 -0600

    Fixed flicker and added some animation

--- Google AI Overview Blocker/Google AI Overview Blocker-2025-08-11.user.js
+++ Google AI Overview Blocker/Google AI Overview Blocker-2025-08-11.user.js
@@ -1,66 +1,100 @@
 // ==UserScript==
 // @name         Google AI Overview Blocker
-// @namespace    https://dme6.github.io
+// @namespace    https://github.com/dme6
 // @version      2025-08-11
 // @description  Encourages you to look for more accurate information before resorting to AI.
 // @author       dme6
 // @match        *://www.google.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
-
-// TODO: Add way to block the element faster
 
 (function() {
     'use strict';
 
-    // const e = document.getElementsByClassName("hdzaWe")[0];
-    const e = document.getElementById("eKIzJc");
+    const ESTR = "eKIzJc";
 
-    if(e){
+    // Initial injection to avoid flicker
+    function init() {
 
-        const div = document.createElement("div");
-        const btn = document.createElement("button");
+        const injection = document.createElement("style");
+        injection.innerText =
+        `
+        #${ESTR} { display: none; }
 
-        const State = {
-            SHOWN: 0,
-            HIDDEN: 1
-        };
+        `;
 
-        let eState = State.HIDDEN;
+        document.head.appendChild(injection);
 
-        e.style.display = "none";
+    }
 
-        div.style.backgroundColor = "rgba(255, 255, 255, 0.08)";
-        div.style.borderRadius = "8px";
-        div.style.marginBottom = "20px";
-        div.style.marginTop = "10px";
+    function block() {
 
-        btn.style.marginLeft = "10px";
-        btn.style.marginTop = "10px";
-        btn.style.marginBottom = "10px";
-        btn.style.padding = "5px";
-        btn.innerText = "Show AI Results";
+        // Another possible location: const e = document.getElementsByClassName("hdzaWe")[0];
+        const e = document.getElementById(ESTR);
 
-        div.appendChild(btn);
-        e.parentElement.appendChild(div)
+        console.log(e);
 
-        btn.addEventListener("click", ev => {
-            switch(eState){
-                case State.HIDDEN: {
-                    e.style.display = "block";
-                    btn.innerText = "Hide AI Results";
-                    eState = State.SHOWN;
-                    break;
+        if(e){
+
+            const div = document.createElement("div");
+            const btn = document.createElement("button");
+
+            const State = {
+                SHOWN: 0,
+                HIDDEN: 1
+            };
+
+            let eState = State.HIDDEN;
+
+            e.style.display = "none";
+
+            div.style.opacity = 0;
+            div.style.backgroundColor = "rgba(255, 255, 255, 0.08)";
+            div.style.borderRadius = "8px";
+            div.style.marginBottom = "15px";
+            div.style.marginTop = "10px";
+
+            btn.style.marginLeft = "10px";
+            btn.style.marginTop = "10px";
+            btn.style.marginBottom = "10px";
+            btn.style.padding = "5px";
+            btn.innerText = "Show AI Results";
+
+            div.appendChild(btn);
+            e.parentElement.appendChild(div)
+
+            const fadeInt = setInterval(() => {
+                if(div.style.opacity < 1) { div.style.opacity = parseFloat(div.style.opacity) + 0.05; }
+                else { clearInterval(fadeInt); }
+                console.log("log");
+            }, 50);
+
+            btn.addEventListener("click", ev => {
+                switch(eState){
+                    case State.HIDDEN: {
+                        e.style.display = "block";
+                        btn.innerText = "Hide AI Results";
+                        eState = State.SHOWN;
+                        break;
+                    }
+                    case State.SHOWN: {
+                        e.style.display = "none";
+                        btn.innerText = "Show AI Results";
+                        eState = State.HIDDEN;
+                        break;
+                    }
                 }
-                case State.SHOWN: {
-                    e.style.display = "none";
-                    btn.innerText = "Show AI Results";
-                    eState = State.HIDDEN;
-                    break;
-                }
-            }
-        });
+            });
+        }
+
+    }
+
+    init();
+
+    document.onreadystatechange = function() {
+        if(document.readyState == "complete") block();
     }
 
 })();
